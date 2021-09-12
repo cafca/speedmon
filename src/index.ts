03693c6491ec923cec46f05f9d5fb41c3d6c09a6
Vincent Ahrend <mail@vincentahrend.com>
2021-09-12 22:33:51 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -12,78 +12,87 @@ let RESOLUTION = 1024 * 1024;
 // Fixed buffer size of node-fetch https requests
 const BUFFER_SIZE = 16 * 1024;
 
+
+/**
+ * Execute request and send transfer timings to observer
+ */
 const runMeasurement = async (url: string) => {
-    const response = await fetch(url, { compress: false });
+  const response = await fetch(url, { compress: false });
 
-    if (!response || !response.ok || !response.body) {
-        throw new Error('Invalid response body');
+  if (!response || !response.ok || !response.body) {
+    throw new Error('Invalid response body');
+  }
+
+  const contentLength = response.headers.get('Content-Length');
+  if (!contentLength || parseInt(contentLength, 10) < 6 * RESOLUTION) {
+    console.error('This payload is too small (min 6 megabytes)');
+    process.exit(1);
+  }
+
+  let index = 0;
+  const chunksPerMeasurement = RESOLUTION / BUFFER_SIZE;
+
+  performance.mark('measurement-interval')
+  for await (const chunk of response.body) {
+    if (index % (chunksPerMeasurement) === 0) {
+      performance.measure('received-data', {
+        start: 'measurement-interval',
+        detail: { index: Math.floor(index / chunksPerMeasurement) }
+      });
+      performance.mark('measurement-interval');
     }
-
-    const contentLength = response.headers.get('Content-Length');
-    if(!contentLength || parseInt(contentLength, 10) < 6 * RESOLUTION) {
-        console.error('This payload is too small (min 6 megabyte)');
-        process.exit(1);
-    }
-
-    let index = 0;
-    const chunksPerMeasurement = RESOLUTION / BUFFER_SIZE;
-
-    performance.mark('measurement-interval')
-    for await (const chunk of response.body) {
-        if (index % (chunksPerMeasurement) === 0) {
-            performance.measure('received-data', {
-                start: 'measurement-interval',
-                detail: { index: Math.floor(index / chunksPerMeasurement) }
-            });
-            performance.mark('measurement-interval');
-        }
-        index += 1;
-    }
+    index += 1;
+  }
 }
 
-const getSpeed = (result: PerformanceEntry): number => {
-    return CONVERSION_FACTOR * RESOLUTION / result.duration;
-}
-
+/**
+ * Request url parameter, observe transfer timings and calculate speed.
+ */
 const run = async (url: string): Promise<number> => {
-    const measurements: number[] = [];
+  const measurements: number[] = [];
 
-    if (!url.startsWith('http')) {
-        url = `https://${url}`;
-    }
+  if (!url.startsWith('http')) {
+    url = `https://${url}`;
+  }
 
-    const obs = new PerformanceObserver((items) => {
-        items.getEntriesByName('received-data').forEach((entry) => {
-            measurements.push(getSpeed(entry))
-        })
-
+  const obs = new PerformanceObserver((items) => {
+    items.getEntriesByName('received-data').forEach((entry) => {
+      measurements.push(CONVERSION_FACTOR * RESOLUTION / entry.duration)
     })
-    obs.observe({ type: 'measure' });
-    try {
-        await runMeasurement(url);
-    } catch (err) {
-        console.error('Measurement failed');
-        console.error(err);
-    }
-    obs.disconnect()
+  })
+  obs.observe({ type: 'measure' });
+  await runMeasurement(url);
+  obs.disconnect()
 
-    const start = Math.round(measurements.length / 4);
-    const end = measurements.length - 2;
-    const filtered = measurements
-        .sort((a,b) => a - b)
-        .slice(start, end)
-    const average = Math.floor(filtered.reduce((prev, cur) => prev + cur, 0) / filtered.length);
-    return average;
+  // Sort all measurements and remove slowest 25% and two fastest results,
+  // then return the average of the rest.
+  const start = Math.round(measurements.length / 4);
+  const end = measurements.length - 2;
+  const filtered = measurements
+    .sort((a, b) => a - b)
+    .slice(start, end)
+
+  if (filtered.length === 0) {
+    throw new Error('Not enough speed measurements recorded');
+  }
+  const sum = filtered.reduce((prev, cur) => prev + cur, 0)
+  const average = Math.floor(sum / filtered.length);
+  return average;
 }
 
 const main = async () => {
-    const url = process.argv[2];
-    if (!url || url.length === 0) {
-        console.error('Usage: speedmon <https://payload.url>')
-        console.error('Payload should be larger than 6 megabytes')
-        process.exit(1);
-    }
+  const url = process.argv[2];
+  if (!url || url.length === 0) {
+    console.error('Usage: speedmon <https://payload.url>')
+    console.error('Payload should be larger than 6 megabytes')
+    process.exit(1);
+  }
+  try {
     const currentSpeed = await run(url);
-    console.log(`${currentSpeed}mps`)
+    console.log(`${currentSpeed} megabits per second`)
+  } catch (err) {
+    console.error('Measurement failed.', err)
+    process.exit(1);
+  }
 }
 main();
